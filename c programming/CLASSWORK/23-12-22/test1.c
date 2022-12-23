741f652d88d314e478e451e73e3961ae30a49ff3
Sagar3431 <sagarvaghasiya343@gmail.com>
2022-12-23 10:56:05 +0530

    Create test1.c

---
+++ c programming/CLASSWORK/23-12-22/test1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+
+{
+	
+
+int no =29;
+char ch ='u';
+float per = 58.78;
+double stock = 5454554545455.53535;
+char name[10]="top tec";
+char mobile[12]="9913338702";
+
+printf("%d\n",no);
+printf("%c\n",ch);
+printf("%.2f",per);
+printf("\n%lf\n",stock);
+printf("%s\n",name);
+printf("%s",mobile);
+
+}
