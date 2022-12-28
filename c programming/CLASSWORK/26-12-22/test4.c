d0b2426428cf4a4ff16a2dfd70c3e608d19ee873
Sagar3431 <sagarvaghasiya343@gmail.com>
2022-12-28 09:33:08 +0530

    Create test4.c

---
+++ c programming/CLASSWORK/26-12-22/test4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+
+{
+	int num1,num2;
+	printf("\nEnter num 1 :");
+	scanf("%d",&num1);
+	printf("\nEnter num 2 :");
+	scanf("%d",&num2);
+	
+	printf("\nAddition %d",(num1+num2));
+	printf("\nsubtraction %d",(num1-num2));
+	printf("\nMultipliction%d",(num1*num2));
+	printf("\nDivision %.2f",((float)num1/num2));
+	
+	
+}
